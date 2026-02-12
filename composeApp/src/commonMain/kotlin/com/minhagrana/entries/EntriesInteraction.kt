5af5c917af054ab52c7ad1d707d0f0ceb542bad0
Leonardo Behnck <89857767+leonardobehnck@users.noreply.github.com>
2026-02-12 17:10:53 -0300

    Add EntriesInteraction sealed class for managing entry interactions
    - Introduced `EntriesInteraction` sealed class to handle various user interactions related to financial entries, including selecting months, adding, and deleting entries.
    - This addition enhances the application's architecture by providing a structured way to manage state changes in the entries feature.

---
+++ composeApp/src/commonMain/kotlin/com/minhagrana/entries/EntriesInteraction.kt
@@ -0,0 +1,21 @@
+package com.minhagrana.entries
+
+import com.minhagrana.entities.Entry
+
+sealed class EntriesInteraction {
+    data object OnMonthSelected : EntriesInteraction()
+
+    data class OnNextMonthSelected(
+        var year: String,
+    ) : EntriesInteraction()
+
+    data class OnPreviousYearSelected(
+        var year: String,
+    ) : EntriesInteraction()
+
+    data object OnEntryDeleted : EntriesInteraction()
+
+    data class OnEntryAdded(
+        val entry: Entry,
+    ) : EntriesInteraction()
+}
