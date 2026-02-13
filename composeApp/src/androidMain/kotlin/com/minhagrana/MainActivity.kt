25c3abe402608fd33e8445397871c4fbeed07782
Leonardo Behnck <89857767+leonardobehnck@users.noreply.github.com>
2026-02-13 17:02:45 -0300

    feat: Update MainActivity to use AppTheme and Surface for improved UI

--- composeApp/src/androidMain/kotlin/com/minhagrana/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/minhagrana/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.minhagrana.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,13 +17,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            App()
+            AppTheme {
+                Surface(
+                    modifier =
+                        Modifier
+                            .imePadding()
+                            .background(MaterialTheme.colorScheme.background),
+                ) {
+                    App()
+                }
+            }
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }
