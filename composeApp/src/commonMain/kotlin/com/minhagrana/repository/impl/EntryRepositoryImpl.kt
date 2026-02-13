23b32cd6b942b67a213fc19ead9de618ae6b8556
Leonardo Behnck <89857767+leonardobehnck@users.noreply.github.com>
2026-02-13 17:12:52 -0300

    feat: Implement repository classes for Category, Entry, User, and Year, providing data access and management functionalities

---
+++ composeApp/src/commonMain/kotlin/com/minhagrana/repository/impl/EntryRepositoryImpl.kt
@@ -0,0 +1,54 @@
+package com.minhagrana.repository.impl
+
+import com.minhagrana.database.DatabaseHelper
+import com.minhagrana.entities.Category
+import com.minhagrana.entities.Entry
+import com.minhagrana.entities.EntryType
+import com.minhagrana.repository.EntryRepository
+import com.minhagrana.repository.MonthRepository
+import kotlinx.coroutines.flow.Flow
+
+class EntryRepositoryImpl(
+    private val databaseHelper: DatabaseHelper,
+    private val monthRepository: MonthRepository,
+) : EntryRepository {
+    override fun getEntriesByMonthIdFlow(monthId: Long): Flow<List<Entry>> = databaseHelper.getEntriesByMonthIdFlow(monthId)
+
+    override suspend fun getEntriesByMonthId(monthId: Long): List<Entry> {
+        val entryEntities = databaseHelper.getEntriesByMonthId(monthId)
+        return entryEntities.map { entity ->
+            val category = databaseHelper.getCategoryById(entity.category_id.toInt()) ?: Category()
+            Entry(
+                id = entity.id.toInt(),
+                uuid = entity.uuid,
+                name = entity.name,
+                value = entity.value_,
+                date = entity.date,
+                repeat = entity.repeat.toInt(),
+                type = EntryType.valueOf(entity.type),
+                category = category,
+            )
+        }
+    }
+
+    override suspend fun getEntryByUuid(uuid: String): Entry? {
+        return null
+    }
+
+    override suspend fun insertEntry(
+        entry: Entry,
+        monthId: Long,
+    ): Long {
+        val entryId = databaseHelper.insertEntry(entry, monthId)
+        monthRepository.recalculateMonthTotals(monthId)
+        return entryId
+    }
+
+    override suspend fun updateEntry(entry: Entry) {
+        databaseHelper.updateEntry(entry)
+    }
+
+    override suspend fun deleteEntry(id: Int) {
+        databaseHelper.deleteEntry(id)
+    }
+}
