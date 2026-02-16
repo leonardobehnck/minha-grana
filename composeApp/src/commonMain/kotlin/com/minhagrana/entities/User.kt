938e4d5b6ba825486244d37d3b55e3aa692d4e0d
Leonardo Behnck <89857767+leonardobehnck@users.noreply.github.com>
2026-02-16 20:30:04 -0300

    refactor: Remove unused properties from User data class for cleaner code

--- composeApp/src/commonMain/kotlin/com/minhagrana/entities/User.kt
+++ composeApp/src/commonMain/kotlin/com/minhagrana/entities/User.kt
@@ -11,16 +11,8 @@ data class UserWrapper(
 
 @Serializable
 data class User(
-    @SerialName("id")
-    val id: Int = -1,
     @SerialName("uuid")
     val uuid: String = "",
     @SerialName("name")
     val name: String = "",
-    @SerialName("email")
-    val email: String = "",
-    @SerialName("password")
-    val password: String = "",
-    @SerialName("balanceVisibility")
-    val balanceVisibility: Boolean = true,
 )
