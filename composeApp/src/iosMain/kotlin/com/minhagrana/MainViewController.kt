64acc0b7bfd7b23b9fb040ed3a304892cba01b7f
Leonardo Behnck <89857767+leonardobehnck@users.noreply.github.com>
2026-02-22 19:24:08 -0300

    refactor: Move keyboard warmup from Compose to native UIKit implementation

--- composeApp/src/iosMain/kotlin/com/minhagrana/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/minhagrana/MainViewController.kt
@@ -1,5 +1,25 @@
 package com.minhagrana
 
 import androidx.compose.ui.window.ComposeUIViewController
+import platform.UIKit.UITextField
+import platform.UIKit.UIViewController
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController(): UIViewController {
+    val controller = ComposeUIViewController { App() }
+
+    warmupKeyboard(controller)
+
+    return controller
+}
+
+private fun warmupKeyboard(controller: UIViewController) {
+    val textField = UITextField()
+    textField.hidden = true
+    controller.view.addSubview(textField)
+
+    platform.darwin.dispatch_async(platform.darwin.dispatch_get_main_queue()) {
+        textField.becomeFirstResponder()
+        textField.resignFirstResponder()
+        textField.removeFromSuperview()
+    }
+}
