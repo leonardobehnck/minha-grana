5d4d49d0f85ec0eee02803557798e5276e80359d
Leonardo Behnck <89857767+leonardobehnck@users.noreply.github.com>
2026-02-22 16:10:51 -0300

    refactor: Remove unused Color import from ProfileScreen

--- composeApp/src/commonMain/kotlin/com/minhagrana/ui/presentation/profile/ProfileScreen.kt
+++ composeApp/src/commonMain/kotlin/com/minhagrana/ui/presentation/profile/ProfileScreen.kt
@@ -20,7 +20,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
