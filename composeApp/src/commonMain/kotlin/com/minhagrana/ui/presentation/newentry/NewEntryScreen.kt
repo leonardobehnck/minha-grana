443b29cb2f1da8b280bce0c4a608f1ab46f70717
Leonardo Behnck <89857767+leonardobehnck@users.noreply.github.com>
2026-02-22 16:10:41 -0300

    refactor: Remove unused Color import from NewEntryScreen

--- composeApp/src/commonMain/kotlin/com/minhagrana/ui/presentation/newentry/NewEntryScreen.kt
+++ composeApp/src/commonMain/kotlin/com/minhagrana/ui/presentation/newentry/NewEntryScreen.kt
@@ -25,7 +25,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
