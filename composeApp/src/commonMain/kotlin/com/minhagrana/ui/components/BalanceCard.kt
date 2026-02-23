ba50704958732423898faec464b101e33a00247a
Leonardo Behnck <89857767+leonardobehnck@users.noreply.github.com>
2026-02-23 15:48:13 -0300

    refactor: Simplify BalanceCard layout and remove preview

--- composeApp/src/commonMain/kotlin/com/minhagrana/ui/components/BalanceCard.kt
+++ composeApp/src/commonMain/kotlin/com/minhagrana/ui/components/BalanceCard.kt
@@ -2,14 +2,12 @@ package com.minhagrana.ui.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.material3.Icon
@@ -18,11 +16,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.minhagrana.ui.balanceColor
 import com.minhagrana.ui.formatDoubleToBRL
-import com.minhagrana.ui.theme.AppTheme
 
 @Composable
 fun BalanceCard(
@@ -32,18 +28,16 @@ fun BalanceCard(
     onClick: () -> Unit = {},
 ) {
     Row(
+        verticalAlignment = Alignment.CenterVertically,
         modifier =
             Modifier
-                .padding(top = 4.dp, bottom = 4.dp)
+                .height(120.dp)
+                .padding(top = 10.dp, bottom = 10.dp)
                 .background(MaterialTheme.colorScheme.surface)
                 .clickable { onClick() }
                 .fillMaxWidth(),
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-            modifier = Modifier.weight(1f),
-        ) {
+        Column(modifier = Modifier.weight(1f),) {
             Text(
                 modifier = Modifier.padding(start = 32.dp),
                 style = MaterialTheme.typography.bodyLarge,
@@ -78,15 +72,5 @@ fun BalanceCard(
                 contentDescription = null,
             )
         }
-    }
-}
-
-@Preview
-@Composable
-fun PreviewBalanceCard() {
-    AppTheme {
-        BalanceCard(
-            title = "Outubro",
-        )
     }
 }
