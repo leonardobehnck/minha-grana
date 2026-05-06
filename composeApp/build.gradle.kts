4477dcd622378057ac4faf331f786ef558acc2e6
Leonardo Behnck <89857767+leonardobehnck@users.noreply.github.com>
2026-05-06 14:07:21 -0300

    chore: bump version code and name

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -89,8 +89,8 @@ android {
             libs.versions.android.targetSdk
                 .get()
                 .toInt()
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
     }
     packaging {
         resources {
